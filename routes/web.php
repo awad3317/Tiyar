d20caa8aab5bcdbed4c4a944e5faa43409e45541
Mohammad_jaffer_alsaadi <Mohammadjaffrsaadix@gmail.com>
2025-06-18 22:48:13 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,19 @@ Route::get('/', [OrderController::class, 'index']);
 Route::get('/order', [OrderController::class, 'create'])->name('order.create');
 Route::post('/order', [OrderController::class, 'store'])->name('order.store');
 
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
 
 Route::post('/github/deploy', function (Request $request){
-    $secret = '14171417Nn'; 
+    $secret = '14171417Nn';
 
         $signature = 'sha256=' . hash_hmac('sha256', $request->getContent(), $secret);
         if (!hash_equals($signature, $request->header('X-Hub-Signature-256'))) {
             Log::warning('Signature mismatch!');
             abort(403, 'Unauthorized');
         }
-        
+
         exec('cd /home/tiyar/htdocs/Tiyar && git pull origin main');
         return response()->json(['status' => 'Deployed']);
 });
