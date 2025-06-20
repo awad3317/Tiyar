0f6c2f79353e09eca8bf479cfe85a91f950be1e9
Mohammad_jaffer_alsaadi <Mohammadjaffrsaadix@gmail.com>
2025-06-20 18:12:06 +0500

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,9 +36,9 @@ class AdminPanelProvider extends PanelProvider
                 'warning' => Color::Amber,
             ])
             ->font('Oswald')
-            ->brandLogo(asset('images/Tiyar2.png'))
+            ->brandLogo(asset('images/tiyar.png'))
             ->brandLogoHeight('3.5rem')
-            ->favicon(asset('images/Tiyar2.png'))
+            ->favicon(asset('images/tiyar.png'))
             ->brandName('Tiyar platform')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
