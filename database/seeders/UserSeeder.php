3284c0a0059be1393eada86452554dcb981275f6
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-06-22 21:30:33 +0300

    add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->truncate();
+
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@example.com',
+                'email_verified_at' => now(),
+                'password' => '12121212', 
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@example.com',
+                'email_verified_at' => now(),
+                'password' => '12121212',
+            ],
+            
+        ]);
+
+    }
+}
