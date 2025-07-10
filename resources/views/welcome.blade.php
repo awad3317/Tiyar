dba54418b31cc007b6d375c88dc5587f4a12c27b
Mohammad_jaffer_alsaadi <Mohammadjaffrsaadix@gmail.com>
2025-07-10 23:05:15 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
 
     <title>تيار للحلول والابتكار | تصميم مواقع وتطبيقات احترافية</title>
-    <link rel="canonical" href="{{ url()->current() }}" />
-    <meta name="robots" content="index, follow">
+{{--    <link rel="canonical" href="{{ url()->current() }}" />--}}
+{{--    <meta name="robots" content="index, follow">--}}
     <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700&display=swap" rel="stylesheet">
 {{--    <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">--}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css">
