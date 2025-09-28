a9862ab71dd6967d9ca3be5d15824fbc13231f94
Mohammad_jaffer_alsaadi <Mohammadjaffrsaadix@gmail.com>
2025-09-28 11:39:37 +0500

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,13 +21,21 @@
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="og:title" content="تيار للحلول والابتكار | تصميم مواقع وتطبيقات احترافية">
     <meta property="og:description" content="شركة تيار متخصصة في تصميم وتطوير المواقع، التطبيقات، والتصاميم الإبداعية. حلول رقمية مبتكرة لأعمالك.">
-    <meta property="og:image" content="{{ asset('images/preview.png') }}">
+    {{-- <meta property="og:image" content="{{ asset('images/preview.png') }}"> --}}
+    <meta property="og:image" content="{{ asset('favicons/favicon-96x96.png') }}">
 
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="تيار للحلول والابتكار | تصميم مواقع وتطبيقات احترافية">
     <meta name="twitter:description" content="شركة تيار متخصصة في تصميم وتطوير المواقع، التطبيقات، والتصاميم الإبداعية. حلول رقمية مبتكرة لأعمالك.">
-    <meta name="twitter:image" content="{{ asset('images/preview.png') }}">
-
+   <link rel="icon" type="image/x-icon" href="{{ asset('favicons/favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicons/favicon-96x96.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicons/web-app-manifest-192x192.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('favicons/apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('favicons/site.webmanifest') }}">
+    <meta name="msapplication-TileImage" content="{{ asset('favicons/favicon-96x96.png') }}">
+    <meta name="msapplication-TileColor" content="#ffffff">
 </head>
 <body data-bs-spy="scroll" data-bs-target="#navbarContent" data-bs-offset="100">
 @if(session('success') || session('error'))
